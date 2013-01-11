0355b427291d8cec5af35675a803d4880cf381fe
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-01-11 18:43:21 -0200

    Flying viking

--- game/main.lua
+++ game/main.lua
@@ -1,9 +1,10 @@
 
 local w,h
-local camera_pos
+local camera_pos = {}
+local player = {}
 local map
 local img
-
+--zuei
 function love.load ()
   w,h = love.graphics.getWidth(), love.graphics.getHeight()
   camera_pos = { x=w/2, y=h/2 }
@@ -16,12 +17,40 @@ function love.load ()
     end
   end
   for i=1,20 do
-    map[10][i].img = img
+    local tile = map[10][i]
+    tile.img = img
+    tile.floor = true
   end
+  player.pos = { x=1, y=9 }
+  player.spd = { x=0, y=0 }
 end
 
 function love.update (dt)
+  player.pos.x = player.pos.x + player.spd.x*dt
+  player.pos.y = player.pos.y + player.spd.y*dt
+end
 
+local movehack = {
+  up = { x=0, y=-1 },
+  down = { x=0, y=1 },
+  left = { x=-1, y=0 },
+  right = { x=1, y=0 }
+}
+
+function love.keypressed (button)
+  local move = movehack[button]
+  if move then
+    player.spd.x = player.spd.x + move.x*3
+    player.spd.y = player.spd.y + move.y*3
+  end
+end
+
+function love.keyreleased (button)
+  local move = movehack[button]
+  if move then
+    player.spd.x = player.spd.x - move.x*3
+    player.spd.y = player.spd.y - move.y*3
+  end
 end
 
 function love.draw ()
@@ -32,5 +61,6 @@ function love.draw ()
       end
     end
   end
+  love.graphics.print("VIKING", 32*(player.pos.x-1), 32*(player.pos.y-1))
 end
 
