64488463b6299fc6a60ccb16c17cc742007afc29
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-01-11 18:28:36 -0200

    Ice platform!

--- game/main.lua
+++ game/main.lua
@@ -1,11 +1,36 @@
 
 local w,h
+local camera_pos
+local map
+local img
 
 function love.load ()
   w,h = love.graphics.getWidth(), love.graphics.getHeight()
+  camera_pos = { x=w/2, y=h/2 }
+  img = love.graphics.newImage "ice.png"
+  map = {}
+  for i=1,15 do
+    map[i] = {}
+    for j=1,20 do
+      map[i][j] = {}
+    end
+  end
+  for i=1,20 do
+    map[10][i].img = img
+  end
+end
+
+function love.update (dt)
+
 end
 
 function love.draw ()
-  love.graphics.print("VIKINGS", w/2, h/2, 0, 1, 1)
+  for i=1,15 do
+    for j=1,20 do
+      if map[i][j].img then
+        love.graphics.draw(map[i][j].img, 32*(j-1), 32*(i-1))
+      end
+    end
+  end
 end
 
