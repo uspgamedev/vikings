3b12e5bdf7fa86adc19ad92ddb09e4563123bdd9
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-02-07 16:46:29 -0200

    Improving the random grid generator.

--- game/mapgenerator.lua
+++ game/mapgenerator.lua
@@ -38,40 +38,48 @@ local empty_block = {
 {'empty', 'empty', 'empty', 'empty'}, }
 
 local function generate_blocks_grid(num_blocks_x, num_blocks_y, blocks)
-  local blocks_grid = {}
+  local blocks_grid = {
+    blocks = blocks,
+    num_x = num_blocks_x,
+    num_y = num_blocks_y
+  }
   for j=1,num_blocks_y do
     blocks_grid[j] = {}
     for i=1,num_blocks_x do
-      blocks_grid[j][i] = (j <= num_blocks_y/2 and empty_block) or blocks[1]
+      blocks_grid[j][i] = (j <= num_blocks_y/2 and empty_block) or blocks[math.random(#blocks)]
     end
   end
   return blocks_grid
+end
+
+local function generate_map_from_grid(grid)
+  return map:new {
+    tileset = grid.blocks.tileset,
+    width   = grid.num_x * grid.blocks.width,
+    height  = grid.num_y * grid.blocks.height,
+    tilegenerator = function (aj, ai)
+      local block_i, block_j = math.floor((ai-1) / grid.blocks.width) + 1, math.floor((aj-1) / grid.blocks.height) + 1
+      local i, j = (ai-1) % grid.blocks.width + 1, (aj-1) % grid.blocks.height + 1
+      return { type = grid[block_j][block_i][j][i] }
+    end
+  }
 end
 
 function random_map()
   local blocks = {
     width  = 4,
     height = 4,
+    tileset = get_tileset(),
     { {'empty', 'empty', 'empty', 'empty'},
       {'empty',   'ice',   'ice', 'empty'},
       {'empty',   'ice',   'ice', 'empty'},
-      {'empty', 'empty', 'empty', 'empty'}, }
+      {'empty', 'empty', 'empty', 'empty'}, },
+    { {'empty', 'empty', 'empty', 'empty'},
+      {'empty',   'ice',   'ice', 'empty'},
+      {'empty',   'ice',   'ice', 'empty'},
+      {  'ice',   'ice',   'ice',   'ice'}, },
   }
 
-  local num_blocks_x = 8
-  local num_blocks_y = 6
-
-  local blocks_grid = generate_blocks_grid(num_blocks_x, num_blocks_y, blocks)
-
-  return map:new {
-    width   = num_blocks_x * blocks.width,
-    height  = num_blocks_y * blocks.height,
-    tileset = get_tileset(),
-    tilegenerator = function (aj, ai)
-      local block_i, block_j = math.floor((ai-1) / blocks.width) + 1, math.floor((aj-1) / blocks.height) + 1
-      i, j = (ai-1) % blocks.width + 1, (aj-1) % blocks.height + 1
-      print(block_i, block_j, j, i)
-      return { type = blocks_grid[block_j][block_i][j][i] }
-    end
-  }
+  local blocks_grid = generate_blocks_grid(7, 4, blocks)
+  return generate_map_from_grid(blocks_grid)
 end
