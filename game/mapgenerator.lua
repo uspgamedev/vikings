2b128ac563ef53b6c9654211a525ce7de9e689b3
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-02-09 01:26:57 -0200

    Blocks -> Grid -> Map functions

--- game/mapgenerator.lua
+++ game/mapgenerator.lua
@@ -33,7 +33,7 @@ end
 
 local empty_block = { '    ', '    ', '    ', '    ', rarity = 0.5 }
 
-local function generate_blocks_grid(num_blocks_x, num_blocks_y, blocks)
+local function generate_grid_from_blocks(num_blocks_x, num_blocks_y, blocks)
   local blocks_grid = {
     blocks = blocks,
     num_x = num_blocks_x,
@@ -53,40 +53,47 @@ local function generate_blocks_grid(num_blocks_x, num_blocks_y, blocks)
       end
     end
   end
-  return blocks_grid
+
+  local grid = {
+    tileset = blocks.tileset,
+    width   = num_blocks_x * blocks.width,
+    height  = num_blocks_y * blocks.height
+  }
+  for block_j = 1,num_blocks_y do
+    for internal_j = 1,blocks.height do
+      grid[(block_j-1) * blocks.height + internal_j] = {}
+      for block_i = 1,num_blocks_x do
+        for internal_i = 1,blocks.width do
+          grid[(block_j-1) * blocks.height + internal_j]
+                 [(block_i-1) * blocks.width  + internal_i] =
+                    blocks_grid[block_j][block_i][internal_j]:sub(internal_i, internal_i)
+        end
+      end
+    end
+  end
+
+  return grid
 end
 
 local function generate_cave_from_grid(grid)
-  local width, height = grid.num_x * grid.blocks.width, grid.num_y * grid.blocks.height
+  local width, height = grid.width, grid.height
   
-  local fullmap = {}
+  local fullmap = grid
   --[[for j=1,height do
     fullmap[j] = {}
     for i=1,width do
       fullmap[j][i] = math.random() < 0.45 and 'I' or ' '
     end
   end]]
-  for block_j = 1,grid.num_y do
-    for internal_j = 1,grid.blocks.height do
-      fullmap[(block_j-1) * grid.blocks.height + internal_j] = {}
-      for block_i = 1,grid.num_x do
-        for internal_i = 1,grid.blocks.width do
-          fullmap[(block_j-1) * grid.blocks.height + internal_j]
-                 [(block_i-1) * grid.blocks.width  + internal_i] =
-                    grid[block_j][block_i][internal_j]:sub(internal_i, internal_i)
-        end
-      end
-    end
-  end
 
   local function walls_nearby( grid, map, j, i, range_j, range_i )
-    local width, height = grid.num_x * grid.blocks.width, grid.num_y * grid.blocks.height
+    local width, height = grid.width, grid.height
     local missing_j = math.max(range_j - j + 1, range_j - height + j, 0)
     local missing_i = math.max(range_i - i + 1, range_i - width  + i, 0)
     local count_walls = (missing_i + 1) * (missing_j + 1) - 1
     for int_j = math.max(j-range_j, 1), math.min(j+range_j, height) do
       for int_i = math.max(i-range_i, 1), math.min(i+range_i, width) do
-        count_walls = count_walls + (grid.blocks.tileset[map[int_j][int_i]].floor and 1 or 0)
+        count_walls = count_walls + (grid.tileset[map[int_j][int_i]].floor and 1 or 0)
       end
     end
     return count_walls
@@ -122,9 +129,9 @@ local function generate_cave_from_grid(grid)
   end
 
   local thecave = map:new {
-    tileset = grid.blocks.tileset,
-    width   = grid.num_x * grid.blocks.width,
-    height  = grid.num_y * grid.blocks.height,
+    tileset = grid.tileset,
+    width   = grid.width,
+    height  = grid.height,
     tilegenerator = function (aj,ai) 
       return { type = fullmap[aj][ai] }
     end
@@ -152,6 +159,6 @@ function random_map()
     blocks.total_rarity = blocks.total_rarity + block.rarity
   end
 
-  local blocks_grid = generate_blocks_grid(18, 8, blocks)
+  local blocks_grid = generate_grid_from_blocks(18, 8, blocks)
   return generate_cave_from_grid(blocks_grid)
 end
