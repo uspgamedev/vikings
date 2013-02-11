b06c83ff3d4814055c353c76ae2479a5fe2f7a2e
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-02-11 16:45:49 -0200

    [sprite] draweffects -> effects

--- game/sprite.lua
+++ game/sprite.lua
@@ -8,7 +8,7 @@ sprite = lux.object.new {
   animation     = nil,
   speed         = 1,
   mirror        = { false, false },
-  draweffects   = nil,
+  effects   = nil,
 
   framestep     = 1,
   frametime     = 0
@@ -16,7 +16,7 @@ sprite = lux.object.new {
 
 sprite.__init = {
   animation   = animation:new{},
-  draweffects = {}
+  effects = {}
 }
 
 function sprite:set_mirror (horizontal, vertical)
@@ -41,20 +41,20 @@ function sprite:update (observer, dt)
     self.frametime = self.frametime - 1/self.animation.fps
   end
   local to_remove = {}
-  for k,effect in pairs(self.draweffects) do
+  for k,effect in pairs(self.effects) do
     if effect:update(dt) then
       table.insert(to_remove, k)
     end
   end
   for _,k in ipairs(to_remove) do
-    self.draweffects[k] = nil
+    self.effects[k] = nil
   end
 end
 
 function sprite:draw (graphics, pos)
   local frame = self.animation.frames[self.framestep]
   self.data:draw(graphics, frame, pos, self.mirror)
-  for _,effect in pairs(self.draweffects) do
+  for _,effect in pairs(self.effects) do
     effect:draw(graphics)
   end
 end
